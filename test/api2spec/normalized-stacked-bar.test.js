738b7d7838e86ab9ebcbe13d8b720cb5f940ef2b
bqxbqx <boqingxin14@gmail.com>
2024-12-29 00:19:30 +0800

    feat: add api2spec test

---
+++ test/api2spec/normalized-stacked-bar.test.js
@@ -0,0 +1,71 @@
+import { api2spec } from "@g2-convert/core";
+import { test } from "vitest";
+import { deepEqual } from "../utils/test-utils";
+
+const value = `
+/**
+ * A recreation of this demo: https://vega.github.io/vega-lite/examples/stacked_bar_h_normalized_labeled.html
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart.coordinate({ transform: [{ type: 'transpose' }] });
+
+chart
+  .interval()
+  .data({
+    type: 'fetch',
+    value:
+      'https://gw.alipayobjects.com/os/bmw-prod/87b2ff47-2a33-4509-869c-dae4cdd81163.csv',
+    transform: [
+      {
+        type: 'filter',
+        callback: (d) => d.year === 2000,
+      },
+    ],
+  })
+  .transform({ type: 'groupX', y: 'sum' })
+  .transform({ type: 'stackY' })
+  .transform({ type: 'normalizeY' })
+  .encode('x', 'age')
+  .encode('y', 'people')
+  .encode('color', 'sex')
+  .scale('color', { type: 'ordinal', range: ['#ca8861', '#675193'] })
+  .axis('y', { labelFormatter: '.0%' })
+  .label({ text: 'people', position: 'inside', fill: 'white' })
+  .tooltip({ channel: 'y', valueFormatter: '.0%' });
+
+chart.render();
+`;
+
+test("Normalized Stacked Bar Chart Test", async () => {
+  const code = await api2spec(value);
+
+  const expected = {
+    type: "interval",
+    autoFit: true,
+    data: {
+      type: "fetch",
+      value:
+        "https://gw.alipayobjects.com/os/bmw-prod/87b2ff47-2a33-4509-869c-dae4cdd81163.csv",
+      transform: [{ type: "filter", callback: (d) => d.year === 2000 }],
+    },
+    encode: { x: "age", y: "people", color: "sex" },
+    transform: [
+      { type: "groupX", y: "sum" },
+      { type: "stackY" },
+      { type: "normalizeY" },
+    ],
+    scale: { color: { type: "ordinal", range: ["#ca8861", "#675193"] } },
+    coordinate: { transform: [{ type: "transpose" }] },
+    axis: { y: { labelFormatter: ".0%" } },
+    labels: [{ text: "people", position: "inside", fill: "white" }],
+    tooltip: { items: [{ channel: "y", valueFormatter: ".0%" }] },
+  };
+
+  deepEqual(code, expected);
+});
---
+++ test/api2spec/slope-chart.test.js
@@ -0,0 +1,80 @@
+import { api2spec } from "@g2-convert/core";
+import { test } from "vitest";
+import { deepEqual } from "../utils/test-utils";
+
+const value = `
+/**
+ * A recreation of this demo: https://observablehq.com/@d3/slope-chart
+ */
+import { Chart } from '@antv/g2';
+
+const chart = new Chart({
+  container: 'container',
+  autoFit: true,
+});
+
+chart
+  .line()
+  .data({
+    type: 'fetch',
+    value:
+      'https://gw.alipayobjects.com/os/bmw-prod/f0bbdcaa-9dbf-4d44-95c1-ac2e26765023.csv',
+  })
+  .encode('x', 'year')
+  .encode('y', 'receipts')
+  .encode('series', 'country')
+  .encode('color', '#000')
+  .scale('x', { type: 'point', padding: 0.25 })
+  .label({
+    text: (d) => \`\${d.country} \${d.receipts}\`,
+    selector: 'first',
+    transform: [{ type: 'overlapDodgeY' }],
+    fontSize: 10,
+    dx: -3,
+    textAlign: 'end',
+  })
+  .label({
+    text: (d) => \`\${d.receipts} \${d.country}\`,
+    selector: 'last',
+    transform: [{ type: 'overlapDodgeY' }],
+    fontSize: 10,
+    dx: 3,
+  });
+
+chart.render();
+`;
+
+test("Slope Chart Test", async () => {
+  const code = await api2spec(value);
+
+  const expected = {
+    type: "line",
+    autoFit: true,
+    data: {
+      type: "fetch",
+      value:
+        "https://gw.alipayobjects.com/os/bmw-prod/f0bbdcaa-9dbf-4d44-95c1-ac2e26765023.csv",
+    },
+    encode: { x: "year", y: "receipts", series: "country", color: "#000" },
+    scale: { x: { type: "point", padding: 0.25 } },
+    labels: [
+      {
+        text: (d) => `${d.country} ${d.receipts}`,
+        selector: "first",
+        transform: [{ type: "overlapDodgeY" }],
+        fontSize: 10,
+        dx: -3,
+        textAlign: "end",
+      },
+      {
+        text: (d) => `${d.receipts} ${d.country}`,
+        selector: "last",
+        transform: [{ type: "overlapDodgeY" }],
+        fontSize: 10,
+        dx: 3,
+      },
+    ],
+  };
+
+  deepEqual(code, expected);
+});
